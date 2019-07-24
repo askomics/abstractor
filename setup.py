a29efc56b149208bb738641074435559198a9285
xgaia <xgaia@gmx.com>
2019-07-24 09:19:17 +0200

    v1.0.4

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='abstractor',
-    version='1.0.3',
+    version='1.0.4',
     description='Abstraction generator for AskOmics, from a distant SPARQL endpoint',
     author='Xavier Garnier',
     author_email='xavier.garnier@irisa.fr',
     url='https://github.com/xgaia/abstractor',
-    download_url='https://github.com/xgaia/abstractor/archive/1.0.3.tar.gz',
+    download_url='https://github.com/xgaia/abstractor/archive/1.0.4.tar.gz',
     install_requires=['SPARQLWrapper'],
     packages=find_packages(),
     license='AGPL',
